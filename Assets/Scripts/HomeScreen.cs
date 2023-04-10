9511328b9e1b7a5ca4da0f3a3574434ba41d3209
Ý Lê Như <imyour2603@gmail.com>
2023-04-10 20:26:00 +0700

    Initialize Home Screen

---
+++ Assets/Scripts/HomeScreen.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HomeScreen : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Login");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
